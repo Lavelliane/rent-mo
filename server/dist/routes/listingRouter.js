647bb4232716ea49943ba016d8a081536f66b52f
Jhury Kevin <jhurylastre432@gmail.com>
2023-09-05 13:13:31 +0800

    added get specific listing route

--- server/dist/routes/listingRouter.js
+++ server/dist/routes/listingRouter.js
@@ -10,6 +10,6 @@ const router = express_1.default.Router();
 router.route('/listing/create').post(auth_1.default, listingController_1.createListing);
 router.route('/listing/my-listings').get(auth_1.default, listingController_1.getListingsByUser);
 router.route('/listings').get(listingController_1.getAllListings);
-router.route('/listing/:id').patch(auth_1.default, listingController_1.updateListing).delete(auth_1.default, listingController_1.deleteListing);
+router.route('/listing/:id').patch(auth_1.default, listingController_1.updateListing).delete(auth_1.default, listingController_1.deleteListing).get(listingController_1.getSpecificListing);
 exports.default = router;
 //# sourceMappingURL=listingRouter.js.map
